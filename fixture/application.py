4e249a6b733ef8dc1d64d02607b3f7b3ca540d0c
Ira <kauira@yandex.ru>
2025-10-14 23:28:17 +0300

    первый тест

---
+++ fixture/application.py
@@ -0,0 +1,35 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+from fixture.session import SessionHelper
+
+class Application:
+
+    def __init__(self, browser,base_url):
+        if browser == "firefox":
+            firefox_options = Options()
+            firefox_binary_path = r'C:\Program Files\Mozilla Firefox\firefox.exe'
+            firefox_options.binary_location = firefox_binary_path
+            self.wd = webdriver.Firefox(options=firefox_options)
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "ie":
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError("Unrecognized browser %s" % browser)
+        self.session = SessionHelper(self)
+        self.base_url = base_url
+
+    def is_valid(self):
+        try:
+            self.wd.current_url
+            return True
+        except:
+            return False
+
+    def open_home_page(self):
+        wd = self.wd
+        wd.get(self.base_url)
+
+    def destroy(self):
+        self.wd.quit()
+
